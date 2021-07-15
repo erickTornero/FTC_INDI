5d423fa239fce9860a083d192487802fa6fe42e7
erickTornero <erickdeivy01@gmail.com>
2021-07-15 03:39:03 -0500

    function controller, runner && installer

---
+++ ftc/run_controller.py
@@ -0,0 +1,27 @@
+import numpy as np
+from wrapper import QuadrotorEnvRos, state_space 
+
+target_pos = np.array([0, 0, 6.0], dtype=np.float32)
+crippled_degree = np.array([1.0, 0.0, 1.0, 1.0], dtype=np.float32)
+state_space = ['rotation_matrix', 'euler', 'position', 'linear_vel', 'angular_vel']
+
+args_init_distribution = {
+        'max_ang_speed': 30,
+        'max_radius_init': 6,
+        'angle_rad_std': 0.6,
+        'angular_speed_mean': 0,
+        'angular_speed_std': 1.0,
+}
+
+env = QuadrotorEnvRos(target_pos, crippled_degree, state_space, 100, **args_init_distribution)
+
+controller = None#Controller()
+
+max_path_length = 1000
+
+obs = env.reset()
+cum_reward = 0
+for i in range(max_path_length):
+    control_signal = controller(obs)
+    obs, reward, done, info,  = env.step(control_signal)
+    cum_reward += reward
---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+setuptools.setup(
+    name="ftc-indi",
+    version="0.0.1",
+    author="Erick Tornero",
+    author_email="erickdeivy01@gmail.com",
+    description="Indi Control for Faulted Rotor in Quadrotors",
+    url="https://github.com/erickTornero/FTC_INDI",
+    scripts=[],
+    packages=setuptools.find_packages(include=['ftc', 'ftc.*', 'wrapper']),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[ 
+        'numpy >= 1.18.1',
+        'tqdm>=4.46.0',
+        'gym >= 0.12.0',
+        'pyyaml >= 5.4.1',
+        'rospkg >= 1.3.0'
+    ]
+)
