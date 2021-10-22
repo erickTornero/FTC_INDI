a41dfe6b849fd0ff28a666898861d85bf07210db
erickTornero <erickdeivy01@gmail.com>
2021-10-22 01:47:36 -0500

    handle folders and rolls, give initial conditions

--- scripts/run.py
+++ scripts/run.py
@@ -1,4 +1,6 @@
 import os
+import json
+import glob
 import numpy as np
 from ftc.utils.gen_trajectories import Trajectory
 from ftc.indi.parameters import Parameters
@@ -10,37 +12,73 @@ from ftc.utils.logger import Logger
 crippled_degree = np.array([1.0, 1.0, 1.0, 1.0], dtype=np.float32)
 state_space = ['rotation_matrix', 'euler', 'position', 'linear_vel', 'angular_vel']
 
-args_init_distribution = {
+config = {
+    "max_path_length": 10000,
+    "nrollouts": 20,
+    'args_init_distribution': {
         'max_radius': 4.2,
         'max_ang_speed': 30,
-        'max_radius_init': 0,
-        'angle_rad_std': 0.0,
-        'angular_speed_mean': 0,
-        'angular_speed_std': 0.0,
+        'max_radius_init': 2.0,
+        'angle_rad_std': 0.6,
+        'angular_speed_mean': 0.0,
+        'angular_speed_std': 6.0,
+    },
+    "trajectory_args"   :   {
+        "wave"          :   'point',
+        "nrounds"       :   2
+    },
+    "inject_failure"    :   {
+        "allow": False,
+        "type": "push",  # "push" or "ornstein"
+        "push_failure_at": 200,
+        "push_new_task": 0.0,
+        "ornstein_uhlenbeck": {
+            "delta": 0.03,
+            "sigma": 0.20, 
+            "ou_a":  0.5,
+            "ou_mu": 0.78, 
+            "seed":  42
+        }
+    }
 }
+zbias = -6.0
 
 quadrotor_parms_path = 'params/quad_parameters.json'
 control_parms_path = 'params/control_parameters_indi.json'
 parameters = Parameters(quadrotor_parms_path, control_parms_path)
+config['params'] = parameters.params
+
 rate = parameters.freq
 Ts = 1/rate
 if parameters.fail_id>=0:
     crippled_degree[parameters.fail_id] = 0.0
 
-env = QuadrotorEnvRos(np.zeros(3, dtype=np.float32), crippled_degree, state_space, rate, **args_init_distribution)
+env = QuadrotorEnvRos(np.zeros(3, dtype=np.float32), crippled_degree, state_space, rate, **config['args_init_distribution'])
 
 controller = INDIController(parameters=parameters, T_sampling=Ts)
 
-max_path_length = 10000
+max_path_length = config['max_path_length']
 
-cum_reward = 0
-trajectory_manager = Trajectory(max_path_length, -3)
-trajectory = trajectory_manager.gen_points('helicoid', 2)
+trajectory_manager = Trajectory(max_path_length, zbias)
+trajectory = trajectory_manager.gen_points(**config['trajectory_args'])
 
-inject_failure = {'allow': False}
-save_paths = './data/rollouts5'
+inject_failure = config['inject_failure']
+save_paths = './data/rollouts11'
 logger  =   Logger(save_paths, 'logtest.txt')
-nrolls = 20
+nrolls = config['nrollouts']
+
+if save_paths is not None:
+    configsfiles    =   glob.glob(os.path.join(save_paths,'*.json'))
+    files_paths     =   glob.glob(os.path.join(save_paths,'*.pkl'))
+
+    assert len(configsfiles) ==0, 'Already the folder is busy, select other'
+    assert len(files_paths)==0, 'Already the folder is busy, select another one'
+    if not os.path.exists(save_paths):
+        os.makedirs(save_paths)
+    
+    with open(os.path.join(save_paths, 'experiment_config.json'), 'w') as fp:
+        json.dump(config, fp, indent=2)
+
 rollouts(
     env, 
     controller, 
