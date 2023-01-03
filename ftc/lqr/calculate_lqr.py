fix: final w_speeds calculation

ftc/lqr/calculate_lqr.py
@@ -1,12 +1,13 @@
-from ast import Param
 from operator import mod
 from typing import List, Union
-from xmlrpc.client import boolean
 import numpy as np
 import control
 from .parameters import Parameters
 from ..utils.state import State
-def get_lqr_matrix(parameters, fail_idx: Union[int, List[int]], DRF_enable: bool):
+from .equilibrium_sim import SolutionQuadrotorWrapper
+def get_lqr_matrix(parameters: Parameters, fail_idx: Union[int, List[int]], DRF_enable: bool):
+    actuator_dynamics = parameters.actuator_dynamics
+    arm_length          =   parameters.arm_length
     if DRF_enable and fail_idx>=0:
         if fail_idx == 0 or fail_idx == 2:
             fail_id = [0, 2]
@@ -29,7 +30,7 @@ def get_lqr_matrix(parameters, fail_idx: Union[int, List[int]], DRF_enable: bool
         w_bar[fail_id] = 0
     else:
         pass
-
+    """
     Iz = parameters.Iz
     Iu = parameters.Iu
     Iv = parameters.Iv
@@ -38,23 +39,36 @@ def get_lqr_matrix(parameters, fail_idx: Union[int, List[int]], DRF_enable: bool
     Av  =   (Iz - Iu)/Iv
     Ip  =   parameters.Ip
     k   =   parameters.act_dyn
-
     a   =   Au * r_bar - Ip * (w_bar[0] - w_bar[1] + w_bar[2] - w_bar[3])/Iu
     b   =   Av * r_bar + Ip * (w_bar[0] - w_bar[1] + w_bar[2] - w_bar[3])/Iv
+    """
+    ixxt    =   parameters.ixxt
+    izzt    =   parameters.izzt
+    ixxb    =   parameters.ixxb
+    izzp    =   parameters.izzp
 
+    a = ((ixxt - izzt)/ixxb) * r_bar - (izzp/ixxb) * (w_bar[0] - w_bar[1] + w_bar[2] - w_bar[3])
     A   =   np.array([
         [0, a, 0, 0],
-        [b, 0, 0, 0],
+        [-a, 0, 0, 0],
         [0, 1, 0, r_bar],
         [-1, 0, -r_bar, 0]
     ])
+    B   =   (arm_length/ixxb) * np.array([
+        [1, 0],
+        [0, 1],
+        [0, 0],
+        [0, 0]
+    ])
 
+    """
     B   =   np.array([
         [1/Iu, 0],
         [0, 1/Iv],
         [0, 0],
         [0, 0]
     ])
+    """
 
     if hasattr(fail_id, '__len__'):
         if (np.sum(fail_id) < 3): # failure 0, 2
@@ -63,7 +77,8 @@ def get_lqr_matrix(parameters, fail_idx: Union[int, List[int]], DRF_enable: bool
             if fail_id >= 0: B[:, 0] = 0
 
     
-    S = k * np.eye(2) # 2x2
+    #S = k * np.eye(2) # 2x2
+    S = (1/actuator_dynamics) * np.eye(2)
 
     tShape = (S.shape[0], A.shape[1]) # (2 x 4)
     AA = np.vstack([
@@ -81,7 +96,29 @@ def get_lqr_matrix(parameters, fail_idx: Union[int, List[int]], DRF_enable: bool
     K, _, _ = control.lqr(AA, BB, Q, R)
     return K
 
+def get_lqr_matrix_improved(parameters: Parameters, fail_idx: int, alpha_ratio: float):
+    solver = SolutionQuadrotorWrapper(
+        mass=parameters.mass,
+        gravity=parameters.gravity,
+        kt=parameters.kt,
+        kf=parameters.kf,
+        dumping=2.75e-3,
+        length_arm=parameters.arm_length,
+        izzt=parameters.izzt,
+        ixxt=parameters.ixxt,
+        izzp=parameters.izzp,
+        ixxb=parameters.ixxb
+    )
+    Ae, Be, solution = solver.get_extended_control_matrixes(
+        fail_idx,
+        alpha_ratio=alpha_ratio,
+        up_time_motor=parameters.actuator_dynamics
+    )
+    Q  =   np.diag([0, 0, 2, 2, 0.0, 0.0])
+    R  =   np.eye(2)
 
+    K, _, _ = control.lqr(Ae, Be, Q, R)
+    return K
     
 
 def calculate_throtle(state, z_ref, vz_ref, parameters):
@@ -113,18 +150,11 @@ class Mixer:
                 fail_id = [0, 2]
             else:
                 fail_id = [1, 3]
-
+        """
         k0     =   parameters.k0
         t0     =   parameters.t0
         s      =   parameters.s/2.0
-        """
-        G0     =   np.array([
-            [0, -1, 0, 1], # moment X
-            [1, 0, -1, 0], # moment Y
-            [1, 1, 1, 1],  # Sum Forces ...ok
-            [1, -1, 1, -1] # moment Z ...ok
-        ]) # torques
-        """
+
         G0  =   np.array([
             [0, 1, 0, -1], # moment X
             [-1, 0, 1, 0], # moment Y
@@ -133,19 +163,29 @@ class Mixer:
         ])
 
         G      =   np.matmul(np.diag([s * k0, s * k0, k0, t0]), G0)
+        """
+        kf      =   parameters.kf
+        kt      =   parameters.kt
+        l_arm   =   parameters.arm_length
+        G       =   np.array([
+            [0,           kf * l_arm, 0,          -kf*l_arm ],
+            [-kf * l_arm, 0,          kf * l_arm,  0        ],
+            [kf,          kf,         kf,          kf       ],
+            [kt * kf,    -kt * kf,    kt * kf,    -kt * kf  ],
+        ])
 
         if type(fail_id) == int:
             if fail_id >= 0: G[:, fail_id] = 0#np.zeros((4, 1))
         elif hasattr(fail_id, '__len__'):
             G[:, fail_id] = 0#np.zeros_like(4, len(fail_id))
         
+        # matmul(G, w^2) = U
         self.G = G
         self.pinvG  =   np.linalg.pinv(G)
         self.w_min  =   parameters.w_min
         self.w_max  =   parameters.w_max
     
     def __call__(self, U: np.ndarray) -> np.ndarray:
-        
         w = np.zeros(4)
         w2 = np.matmul(self.pinvG, U.reshape(-1, 1))
 

ftc/lqr/controller.py
@@ -1,14 +1,14 @@
 from typing import Dict, Optional
 import numpy as np
 from ftc.indi.controller import DiscreteTimeDerivative
-from ftc.lqr.reduced_lqr import ReducedAttitudeController
+from ftc.lqr.reduced_lqr import ReducedAttitudeController, ReducedAttitudeControllerImproved
 
 from ftc.utils.filters import LowpassFilter
 from ftc.utils.transforms import pos_invert_yz
 from ftc.base_controller import BaseController
 from ftc.lqr.parameters import Parameters
 from wrapper.state_space import StateSpaceRobots
-from ftc.lqr.calculate_lqr import Mixer, get_lqr_matrix, FlotCalculator
+from ftc.lqr.calculate_lqr import Mixer, get_lqr_matrix, FlotCalculator, get_lqr_matrix_improved
 from ftc.utils.state import State
 from ftc.utils.inputs import Inputs
 from ftc.lqr.poscontrol import PositionControl
@@ -24,6 +24,8 @@ class LQRController(BaseController):
         self.parameters.k_lqr1 = get_lqr_matrix(parameters, 1, False)
         self.parameters.k_lqr2 = get_lqr_matrix(parameters, 2, False)
         self.parameters.k_lqr3 = get_lqr_matrix(parameters, 3, False)
+        #self.parameters.k_lqr2 = get_lqr_matrix_improved(parameters, 2, 0.5)
+        #self.parameters.k_lqr3 = get_lqr_matrix_improved(parameters, 3, 0.5)
         # double rotor failure
         self.parameters.k_lqr02 = get_lqr_matrix(parameters, 0, True)
         self.parameters.k_lqr13 = get_lqr_matrix(parameters, 1, True)
@@ -31,7 +33,8 @@ class LQRController(BaseController):
         self.z_target_filter   = LowpassFilter(1, parameters.t_filter, T_sampling)
         self.z_target_derivator =   DiscreteTimeDerivative(T_sampling)
         self.flot_calculator    =   FlotCalculator(parameters)
-        self.reduced_att_controller =   ReducedAttitudeController(parameters)
+        #self.reduced_att_controller =   ReducedAttitudeController(parameters)
+        self.reduced_att_controller =   ReducedAttitudeControllerImproved(parameters)
         self.mixer                  =   Mixer(parameters)
 
         self.errorInt = np.zeros(3, dtype=np.float32)

ftc/lqr/equilibrium_sim.py
@@ -77,7 +77,7 @@ class SolutionQuadrotorWrapper:
         return root, cl
 
 
-    def get_control_matrixes(self, failed_rotor: int, alpha_ratio: float) -> Tuple[np.ndarray, np.ndarray]:
+    def get_control_matrixes(self, failed_rotor: int, alpha_ratio: float) -> Tuple[np.ndarray, np.ndarray, SolutionXBar]:
         root, _ = self.__call__(failed_rotor, alpha_ratio)
         solution = SolutionXBar(*root)
         # from equation 28
@@ -97,16 +97,16 @@ class SolutionQuadrotorWrapper:
             [0, 0],
             [0, 0]
         ])
-        return A, B
+        return A, B, solution
 
     def get_extended_control_matrixes(self, failed_rotor: int, alpha_ratio: float, up_time_motor: float):
-        A, B = self.get_control_matrixes(failed_rotor, alpha_ratio)
+        A, B, solution = self.get_control_matrixes(failed_rotor, alpha_ratio)
         S = np.eye(2) * 1/up_time_motor
         Ae_upper = np.concatenate([A, B], axis=1)
         Ae_lower = np.concatenate([np.zeros((S.shape[0], A.shape[1])), -S], axis=1)
         Ae = np.concatenate([Ae_upper, Ae_lower], axis=0)
         Be = np.concatenate([np.zeros((A.shape[0], B.shape[1])), S], axis=0)
-        return Ae, Be
+        return Ae, Be, solution
 
 
     def get_initial_conditions(

ftc/lqr/reduced_lqr.py
@@ -2,7 +2,8 @@ import numpy as np
 from ftc.lqr.parameters import Parameters
 from ftc.utils.state import State
 from math import cos, sin
-
+import control
+from .equilibrium_sim import SolutionQuadrotorWrapper
 class ReducedAttitudeController:
     def __init__(self, parameters: Parameters):
         K = parameters.k_lqrff
@@ -87,6 +88,123 @@ class ReducedAttitudeController:
         U[2] = T_ref            # total sum of force
         U[3] = Mz_ref           # total moment
         return U
+
+class ReducedAttitudeControllerImproved:
+    def __init__(self, parameters: Parameters, alpha_ratio=0.5):
+        self.fail_id = parameters.fail_id
+        self.parameters = parameters
+        #
+        self.u0 = 0
+        self.v0 = 0
+        solver = SolutionQuadrotorWrapper(
+            mass=parameters.mass,
+            gravity=parameters.gravity,
+            kt=parameters.kt,
+            kf=parameters.kf,
+            dumping=2.75e-3,
+            length_arm=parameters.arm_length,
+            izzt=parameters.izzt,
+            ixxt=parameters.ixxt,
+            izzp=parameters.izzp,
+            ixxb=parameters.ixxb
+        )
+        Ae, Be, solution = solver.get_extended_control_matrixes(
+            parameters.fail_id + 1,
+            alpha_ratio=alpha_ratio,
+            up_time_motor=parameters.actuator_dynamics
+        )
+        self.equilibrium_state = solution
+        self.u1_equilibrium = self.equilibrium_state.f3 - self.equilibrium_state.f1
+        self.u2_equilibrium = self.equilibrium_state.f2 - self.equilibrium_state.f4
+
+        Q  =   np.diag([0, 0, 2, 2, 0.0, 0.0])
+        R  =   np.eye(2)
+
+        K, _, _ = control.lqr(Ae, Be, Q, R)
+
+        self.K_lqr = K
+        # other params
+        self.Iz =   parameters.Iz
+        self.kf =   parameters.kf
+        self.kt =   parameters.kt
+        self.length_arm = parameters.arm_length
+        self.ixxb = parameters.ixxb
+
+    def __call__(self, state: State, n_des: np.ndarray, f_ref: float, r_cmd: float) -> np.ndarray:
+        # parameters from state
+        # attitude, omegaf, w_speeds
+        phi = state.att[0]
+        theta = state.att[1]
+        psi = state.att[2]
+
+        u = state.omegaf[0]# tmp[0, 0] # u1 # assumption this is pqr is it in body frame?
+        v = state.omegaf[1]# tmp[1, 0] # u2
+        r = state.omegaf[2]
+
+        R_IB = np.array([
+            [cos(psi) * cos(theta), cos(psi) * sin(theta) * sin(phi) - sin(psi) * cos(phi), cos(psi) * sin(theta) * cos(phi) + sin(psi) * sin(phi)],
+            [sin(psi) * cos(theta), sin(psi) * sin(theta) * sin(phi) + cos(psi) * cos(phi), sin(psi) * sin(theta) * cos(phi) - cos(psi) * sin(phi)],
+            [-sin(theta), cos(theta) * sin(phi), cos(theta) * cos(phi)]
+        ])
+        u, v, _ = np.matmul(R_IB, np.array(state.omegaf).reshape(-1, 1)).flatten().tolist()
+
+        h_des = np.linalg.solve(R_IB, n_des)
+        #import pdb; pdb.set_trace()
+        #eta = np.matmul(self.R_xy_uv, h[:2].reshape(-1, 1))
+        eta = h_des[:2]#.reshape(-1, 1)
+
+        w_speeds = state.w_speeds # s: 0.34?
+        # torques xy in body axis
+        #Muv0 = self.kf * self.length_arm * np.array([w_speeds[1]**2 - w_speeds[3]**2, w_speeds[2]**2 - w_speeds[0]**2])
+        Muv0 = self.kf * np.array([w_speeds[1]**2 - w_speeds[3]**2, w_speeds[2]**2 - 405**2])
+        Muv0 = -self.kf * np.array([w_speeds[2]**2 - w_speeds[0]**2, w_speeds[1]**2 - 405**2])
+
+        Muv0 = self.kf * np.array([w_speeds[2] - w_speeds[0]**2, w_speeds[1]**2 - w_speeds[3]**2])
+        ###Muv0 -= np.array([self.u1_equilibrium, self.u2_equilibrium])
+        ##Muv0 = self.kf * np.array([w_speeds[1]**2 - w_speeds[3]**2, w_speeds[2] - w_speeds[0]**2]) #...from equation 12 and 13
+        ##Muv0 -= np.array([self.u2_equilibrium, self.u1_equilibrium, ])
+        #Muv0 = self.kf * np.array([w_speeds[2]**2 - w_speeds[0]**2, w_speeds[1]**2 - 405**2])
+        #Muv0 = self.k0 * self.s * np.array([[w_speeds[3]**2 - w_speeds[1]**2], [w_speeds[0]**2 - w_speeds[2]**2]])
+        state_eq_pt = np.array([
+            u - self.equilibrium_state.p,
+            v - self.equilibrium_state.q,
+            eta[0] - self.equilibrium_state.nx,
+            eta[1] - self.equilibrium_state.ny,
+            Muv0[0] - self.u1_equilibrium,
+            Muv0[1] - self.u2_equilibrium,
+        ]).reshape(-1, 1)
+        #Muv = - np.matmul(self.K_lqr, np.array([u - self.u0, v - self.v0, eta[0, 0], eta[1, 0], Muv0[0, 0], Muv0[1, 0]]).reshape(-1, 1))
+        Muv = -np.matmul(self.K_lqr, state_eq_pt) # control givven a point
+        T_ref = f_ref
+        #Mz_ref = self.yrc_kp_r * (r - r_cmd) * self.Iz
+        #Mz_ref = self.yrc_kp_r * r # improved computation of the momment z #improve
+        #must be computed with damping
+        Mz_ref = 2.75e-3 * r
+
+        ## Transform to torques
+        # Muv(0): f2 - f4
+        # Muv(1): f3 - f1 
+        # to real forces
+        # Muv(0): f2 - f4 + (f2_bar - f4_bar)
+        # Muv(1): f2 - f4 + (f2_bar - f4_bar)
+        # transform to torques
+        # Tx = (f2 - f4) * l_arm
+        # Ty = (f3 - f1) * l_arm
+        #####
+        # u1: f3 - f1 + (f3_bar - f1_bar)
+        # u2: f2 - f4 + (f2_bar - f4_bar)
+        # Tx = (u2 + u2_bar)*length_arm
+        # Ty = (u1 + u1_bar)*length_arm
+        Tx = (Muv[1, 0] + self.u2_equilibrium) * self.length_arm
+        Ty = (Muv[0, 0] + self.u1_equilibrium) * self.length_arm
+
+        U = np.zeros(4)
+        U[0] = Tx #u1 (f2  - f4) 
+        U[1] = Ty #u2 (f3 - f1)
+        U[2] = T_ref            # total sum of force
+        U[3] = Mz_ref           # total moment
+        return U
+
 
 if __name__ == "__main__":
     from ftc.lqr.calculate_lqr import get_lqr_matrix

ftc/lqr/sim_lqr.py
@@ -1,5 +1,6 @@
 import numpy as np
 from typing import Tuple
+from scipy.optimize import fsolve
 
 def get_n(nz: float) -> np.ndarray:
     assert np.abs(nz) <= 1.0, "invalid value of nz"
@@ -84,13 +85,92 @@ def get_wb(n: np.ndarray, yaw_speed: float) -> np.ndarray:
     wby = yaw_speed * ny/nz
     return np.array([wbx, wby, yaw_speed])
 
+def solve_generic(
+    *,
+    mass: float,
+    gravity: float,
+    alpha: float,
+    kt: float,
+    kf: float,
+    dumping: float,
+    length_arm: float,
+    izzt: float,
+    ixxt: float,
+    izzp: float,
+    fail_rotor: int
+):
+    
+    """
+        x[0]: nx
+        x[1]: ny
+        x[2]: nz
+        x[3]: f1
+        x[4]: f2
+        x[5]: f3
+        x[6]: f4
+        x[7]: sF
+        x[8]: E
+        x[9]: p
+        x[10]: q
+        x[11]: r
+        x[12]: w1
+        x[13]: w2
+        x[14]: w3
+        x[15]: w4
+    """
+    # general equations, i.e independent from which rotor is failed
+    def get_equations(failed_rotor: int, alpha_ratio):
+        def system_dynamics(x):
+            general_eqs = [
+                x[7] * x[2] - mass * gravity,   # from equation #1 ..reference in paper (18)
+                x[0] - x[8] * x[9],             # from equation #2 ..reference paper (16)
+                x[1] - x[8] * x[10],            # from equation #3
+                x[2] - x[8] * x[11],            # from equation #4
+                x[11] - (kf*kt/dumping) * (x[12]**2 - x[13]**2 + x[14]**2 - x[15]**2), # from equation #5 ..reference in paper (21)
+                kf * (x[13]**2 - x[15]**2) * length_arm - (izzt - ixxt) * x[10] * x[11] - izzp * x[9] * (x[12] + x[13] + x[14] + x[15]), # from equation #6 ..reference in paper (12)
+                kf * (x[14]**2 - x[12]**2) * length_arm + (izzt - ixxt) * x[9] * x[11] + izzp * x[9] * (x[12] + x[13] + x[14] + x[15]), # from equation #7 ..reference in paper (13)
+                x[3] - kf * x[12]**2,           # from equation #8 ..reference in paper (10)
+                x[4] - kf * x[13]**2,           # from equation #9   ""
+                x[5] - kf * x[14]**2,           # from equation #10  ""
+                x[6] - kf * x[16]**2,           # from equation #11  ""
+                x[7] - x[3] - x[4] - x[5] - x[6], # from equation #12, reference in paper (18-)
+            ]
+            if failed_rotor == 1:
+                particular_equations = [
+
+                ]
+            elif failed_rotor == 2:
+                pass
+            elif failed_rotor == 3:
+                pass
+            elif failed_rotor == 4:
+                particular_equations = [
+                    x[15] - 0,
+                    x[4] - alpha_ratio * x[3],
+                    x[3] - x[5],
+                    x[9] - 0,
+                ]
+            else:
+                raise ValueError("Rotors are available in index [1-4]")
+            eqs = general_eqs + particular_equations
+            
+            return eqs
+        return system_dynamics
+
+    dynamics = get_equations(fail_rotor, alpha)
+    root = fsolve(dynamics, [0.0, 0.0, 1.0, 2.0, 2.0, 2.0, 2.0, 8.0, 0.05, 2, 2, 20, 400, 400, 400, 400])
+    evaluation = dynamics(root)
+    return root, evaluation
+
 def solve(mass, g, p, kt, kf, dump, l_arm, izzt, ixxt, izzp, fail_rotor=4):
     """
         Fail rotor from [1 to 4]
+        We have 16 variables
     """
     from scipy.optimize import fsolve
     """
-        x[0]: fb where sum forces must always be: (2+p)fb # correspond to a failed-free rotor
+    Variables
+        x[0]: 
         x[1]: nz
         x[2]: r_bar
         x[3]: wi // wi == (wi+2), wi+1 = p*wi # correspond to a failed-free rotor
@@ -100,6 +180,7 @@ def solve(mass, g, p, kt, kf, dump, l_arm, izzt, ixxt, izzp, fail_rotor=4):
         x[7]: q_bar
         x[8]: e, from equation 16 where n_bar = e * [p_bar, q_bar, r_bar]
     """
+
     def func_rotor_4(x):
         """
         if rotor 4 fails